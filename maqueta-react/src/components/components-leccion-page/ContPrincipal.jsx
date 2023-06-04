1294e113b01c03cdd9e9d75c8e8b68b358cf8ad5
Xenock20 <juliosaboredo@gmail.com>
2023-06-03 22:30:56 -0300

    Add routing in pageLeccion

---
+++ maqueta-react/src/components/components-leccion-page/ContPrincipal.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ContPrincipal({text}) {
+  return (
+    <div className='cont-principal'>{text}</div>
+  )
+}
--- maqueta-react/src/pages/LeccionPage.jsx
+++ maqueta-react/src/pages/LeccionPage.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./../components/components-pract-page/NavBar";
 import ContTop from "./../components/components-pract-page/ContTop";
 import ButtonCont from "./../components/components-pract-page/ButtonCont";
 import { useParams } from "react-router";
+import { lecciones } from "./../Contenido/Lecciones";
+import ContPrincipal from "../components/components-leccion-page/ContPrincipal";
+import ContSecundario from "../components/components-leccion-page/ContSecundario";
 
 export default function LeccionPage() {
   const { id } = useParams();
+  const [titulo, setTitulo] = useState("");
+  const [contPrincipal, setContPrincipal] = useState("");
+  const [contSecundario, setContSecundario] = useState([]);
 
-  console.log(id);
+  useEffect(() => {
+    const leccionCopia = lecciones[id]
+    
+    setTitulo(leccionCopia.titulo)
+    setContPrincipal(leccionCopia.contenido.contPrincipal)
+    setContSecundario(leccionCopia.contenido.contSecundario)
+  }, []);
 
   return (
     <div className="home-page">
@@ -15,8 +27,9 @@ export default function LeccionPage() {
       <div className="cont-home">
         <ContTop></ContTop>
         <div className="leccion-cont">
-          <h1>Titulo de la leccion</h1>
-          <p></p>
+          <h1>{titulo}</h1>
+          <ContPrincipal text={contPrincipal}></ContPrincipal>
+          <ContSecundario cont={contSecundario}></ContSecundario>
         </div>
         <ButtonCont></ButtonCont>
       </div>
