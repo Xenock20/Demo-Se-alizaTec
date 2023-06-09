dfcfffe2ec5f81a8ba9ee56632a50d86825749f4
Lukitas <flucasyoel@outlook.es>
2023-06-09 08:07:25 -0300

    fix style

--- maqueta-react/src/pages/EndPage.jsx
+++ maqueta-react/src/pages/EndPage.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './style/EndPage.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style/EndPage.css";
+
 export const EndPage = () => {
   return (
-    <> <div className='caja-completa'>
-        <h1>FELICIDADES!!</h1>
-        <p>Has completado la Demo de SeñalizaTec</p>
-        <p>Muchas gracias por jugar</p>
-        <Link to={"/"}>Regresar</Link>
+    <>
+      
+        <div className="caja-completa">
+        <div className="caja-fondo">
+          <h1>FELICIDADES!</h1>
+          <p>Has completado la Demo de <span style={{fontWeight : '600'}}>Señaliza<span style={{color : '#de4a97' }}>Tec</span></span></p>
+          <p>Muchas gracias por jugar</p>
+          <Link to={"/"}>REGRESAR INICIO</Link>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default EndPage
+export default EndPage;
