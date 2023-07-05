77bc6b983c08b5999740bfe3facb843cd6325e43
Lukitas <flucasyoel@outlook.es>
2023-07-05 17:44:45 -0300

    Add style camino de niveles

--- maqueta-react/src/components/components-CaminoNiveles/Boton.jsx
+++ maqueta-react/src/components/components-CaminoNiveles/Boton.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "animate.css";
 import { nivel } from "../../Contenido/Niveles";
 import { Link } from "react-router-dom";
+import start from "../../assets/startOne.png";
 
 const BotonesLevels = () => {
   const [mostrarInfo, setMostrarInfo] = useState(false);
@@ -18,7 +19,123 @@ const BotonesLevels = () => {
   return (
     <>
       <div className="levels-path">
-        {levels.map((e, index) => {
+        <div className="path-cap-one">
+          <div className="title-dif-one">
+            <div className="conjunto">
+              <div>
+                <img src={start} className="start" alt="Una Estrella" />
+              </div>
+              <div className="sub-cont">
+                <h1 className="title-one">Facil</h1>
+              </div>
+            </div>
+          </div>
+          {levels.map((e, index) => {
+            const descripcion = nivel[index].descripcion;
+            const direccion = nivel[index].link;
+            const position = nivel[index].position;
+            const cap = nivel[index].cap;
+            const positionInfo = nivel[index].positionInfo;
+
+            if (cap === 1) {
+              return (
+                <div className="btn-level-cont">
+                  <button
+                    className={`btn-niveles ${position}`}
+                    onClick={() => {
+                      handleClick(descripcion, direccion);
+                    }}
+                    key={index}
+                  >
+                    {index + 1}
+                  </button>
+                  {mostrarInfo && (
+                    <div className={`cont-info-level ${positionInfo}`}>
+                      <p className="cont-p-level">{informacion}</p>
+                      <Link
+                        to={link}
+                        className={`link-level ${
+                          link.length === 0 ? "prox" : ""
+                        }`}
+                      >
+                        Empezar
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              );
+            }
+          })}
+        </div>
+        <div className="path-cap-two">
+          <div className="title-dif-two">
+            <div className="conjunto">
+              <div>
+                <img src={start} className="start" alt="Dos Estrellas" />
+                <img src={start} className="start" alt="Dos Estrellas" />
+              </div>
+              <div className="sub-cont">
+                <h1 className="title-two">Medio</h1>
+              </div>
+            </div>
+          </div>
+          {levels.map((e, index) => {
+            const descripcion = nivel[index].descripcion;
+            const direccion = nivel[index].link;
+            const position = nivel[index].position;
+            const cap = nivel[index].cap;
+
+            if (cap === 2) {
+              return (
+                <button
+                  className={`btn-niveles ${position}`}
+                  onClick={() => {
+                    handleClick(descripcion, direccion);
+                  }}
+                  key={index}
+                >
+                  {index + 1}
+                </button>
+              );
+            }
+          })}
+        </div>
+        <div className="path-cap-tree">
+          <div className="title-dif-tree">
+            <div className="conjunto">
+              <div>
+                <img src={start} alt="Tres Estrellas" />
+                <img src={start} alt="Tres Estrellas" />
+                <img src={start} alt="Tres Estrellas" />
+              </div>
+              <div className="sub-cont">
+                <h1 className="title-tree">Dificil</h1>
+              </div>
+            </div>
+          </div>
+          {levels.map((e, index) => {
+            const descripcion = nivel[index].descripcion;
+            const direccion = nivel[index].link;
+            const position = nivel[index].position;
+            const cap = nivel[index].cap;
+
+            if (cap === 3) {
+              return (
+                <button
+                  className={`btn-niveles ${position}`}
+                  onClick={() => {
+                    handleClick(descripcion, direccion);
+                  }}
+                  key={index}
+                >
+                  {index + 1}
+                </button>
+              );
+            }
+          })}
+        </div>
+
+        {/* {levels.map((e, index) => {
           const descripcion = nivel[index].descripcion;
           const direccion = nivel[index].link;
           const position = nivel[index].position;
@@ -34,24 +151,8 @@ const BotonesLevels = () => {
               {index + 1}
             </button>
           );
-        })}
+        })} */}
       </div>
-
-      {mostrarInfo && (
-        <div
-          className={`cont-info-level ${
-            mostrarInfo ? "animate__animated animate__bounceInRight" : ""
-          }`}
-        >
-          <p className="cont-p-level">{informacion}</p>
-          <Link
-            to={link}
-            className={`link-level ${link.length === 0 ? "prox" : ""}`}
-          >
-            Empezar
-          </Link>
-        </div>
-      )}
     </>
   );
 };
