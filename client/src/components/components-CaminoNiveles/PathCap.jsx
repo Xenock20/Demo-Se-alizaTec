81e9c74af4b03ee07f60e18664f4939a29ea812f
Xenock20 <juliosaboredo@gmail.com>
2023-09-14 14:12:22 -0300

    Fix style home

--- client/src/components/components-CaminoNiveles/PathCap.jsx
+++ client/src/components/components-CaminoNiveles/PathCap.jsx
@@ -10,16 +10,16 @@ export default function PathCap({ seccion }) {
   const [infoId, setInfoId] = useState(0);
   const [informacion, setInformacion] = useState();
   const [link, setLink] = useState();
-  const [levels, setLevels] = useState([]);
   const [animateBack, setAnimateBack] = useState("");
   const [animateIntro, setAnimateIntro] = useState("");
   const [title, setTitle] = useState("");
-  const [href, setHref] = useState("")
+  const [href, setHref] = useState("");
+  const [dificult, setDificult] = useState([]);
 
   useEffect(() => {
-    setLevels(seccion.levels);
     setTitle(seccion.titulo);
-    setHref(seccion.href)
+    setHref(seccion.href);
+    setDificult(seccion.dificultad);
   }, []);
 
   const handleClick = (descripcion, direccion, id, postInfo) => {
@@ -46,53 +46,65 @@ export default function PathCap({ seccion }) {
   };
 
   return (
-    <div className="path-cap" id={href}>
-      <div className="title-dif">
-            <h1 className="title">{title}</h1>
-      </div>
-      {levels.map((e, index) => {
-        const descripcion = e.descripcion;
-        const direccion = e.link;
-        const position = e.position;
-        const positionInfo = e.positionInfo;
-        const id = e.id;
-        const unlockLevel = nivelesDesbloqueados.includes(id);
-
+    <div id={href}>
+      <h1 className="title-seccion">{title}</h1>
+      {dificult.map((e, i) => {
+        const { title, levels } = e;
         return (
-          <div className={`btn-level-cont-${position}`} key={id}>
-            <button
-              className={
-                unlockLevel
-                  ? `btn-niveles-${position}`
-                  : `btn-niveles-${position} block`
-              }
-              onClick={() => {
-                handleClick(descripcion, direccion, id, positionInfo);
-              }}
-            >
-              {!unlockLevel ? (
-                <object data={BlockIcon} className="block-icon">
-                  {" "}
-                </object>
-              ) : (
-                index + 1
-              )}
-            </button>
-            {id === infoId && (
-              <div
-                className={`cont-info-level ${positionInfo} animate__animated ${
-                  mostrarInfo && id === infoId ? animateIntro : animateBack
-                }`}
-              >
-                <p className="cont-p-level">{informacion}</p>
-                <Link
-                  to={link}
-                  className={`link-level ${link.length === 0 ? "prox" : ""}`}
-                >
-                  Empezar
-                </Link>
-              </div>
-            )}
+          <div className="path-cap" key={i}>
+            <div className="title-dif">
+              <h1 className="title">{title}</h1>
+            </div>
+            {levels.map((e, index) => {
+              const descripcion = e.descripcion;
+              const direccion = e.link;
+              const position = e.position;
+              const positionInfo = e.positionInfo;
+              const id = e.id;
+              const unlockLevel = nivelesDesbloqueados.includes(id);
+
+              return (
+                <div className={`btn-level-cont-${position}`} key={id}>
+                  <button
+                    className={
+                      unlockLevel
+                        ? `btn-niveles-${position}`
+                        : `btn-niveles-${position} block`
+                    }
+                    onClick={() => {
+                      handleClick(descripcion, direccion, id, positionInfo);
+                    }}
+                  >
+                    {!unlockLevel ? (
+                      <object data={BlockIcon} className="block-icon">
+                        {" "}
+                      </object>
+                    ) : (
+                      index + 1
+                    )}
+                  </button>
+                  {id === infoId && (
+                    <div
+                      className={`cont-info-level ${positionInfo} animate__animated ${
+                        mostrarInfo && id === infoId
+                          ? animateIntro
+                          : animateBack
+                      }`}
+                    >
+                      <p className="cont-p-level">{informacion}</p>
+                      <Link
+                        to={link}
+                        className={`link-level ${
+                          link.length === 0 ? "prox" : ""
+                        }`}
+                      >
+                        Empezar
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
           </div>
         );
       })}
