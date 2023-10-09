c24a11ac23d825fb71509fc30f355b3061cb6aea
Lukitas <flucasyoel@outlook.es>
2023-10-09 18:12:33 -0300

    Add BlockAuthenticated

--- client/src/components/unAuthenticated/UnAuthenticated.jsx
+++ client/src/components/unAuthenticated/UnAuthenticated.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 
+import "./unauthenticated.css";
+import { Link } from "react-router-dom";
+
 const UnAuthenticated = () => {
   return (
-    <div>
-      <h1 style={{ textAlign: "center", color: "black" }}>Upss.. Lo siento!</h1>
+    <div className="pag-box">
+      <div className="pag-box-cont">
+        <h1>Upss.. Lo siento!</h1>
 
-      <h2>No estas authenticado para ver esta pagina</h2>
+        <h2>No estas authenticado para ver esta pagina</h2>
+        <Link
+          className="nav-link"
+          style={{
+            width: "160px",
+            height: "30px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: "5px",
+
+            padding: "5px",
+          }}
+          to={"/login"}
+        >
+          Ingresar
+        </Link>
+      </div>
     </div>
   );
 };
