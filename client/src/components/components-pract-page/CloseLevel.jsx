fbe338f1b41559a1a4e426e54ae7359b51ee95a0
imalegomez <imalegomez2004@gmail.com>
2023-11-10 09:01:44 -0300

    add sound

--- client/src/components/components-pract-page/CloseLevel.jsx
+++ client/src/components/components-pract-page/CloseLevel.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import cross from "../../assets/cross.png";
+import next from "../../assets/sounds/next.wav";
 import { Link } from "react-router-dom";
 
 export default function CloseLevel() {
   const link = "/home";
+
+  const reproducirSonido = () =>{
+    const audio = new Audio(next);
+    audio.play();
+  }
+
   return (
     <div className="close-level">
-      <Link style={{ all: "unset", cursor:"pointer" }} to={link}>
+      <Link style={{ all: "unset", cursor:"pointer" }} to={link} onClick={reproducirSonido}>
         <img src={cross} />
       </Link>
     </div>
--- client/src/components/components-pract-page/ReturnBtn.jsx
+++ client/src/components/components-pract-page/ReturnBtn.jsx
@@ -1,14 +1,20 @@
 import React, {useContext} from "react";
 import rowLeft from "../../assets/rowlefth.png";
 import { Link } from "react-router-dom";
+import next from "../../assets/sounds/next.wav";
 import { UserContext } from "../../context/UserProvider";
 
 export default function ReturnBtn({ link }) {
 
   const { desincrementarBarra } = useContext(UserContext)
 
+  const reproducirSonido = () =>{
+    const audio = new Audio(next);
+    audio.play();
+  }
+
   return (
-    <Link to={link} className="return-btn btn" onClick={desincrementarBarra}>
+    <Link to={link} className="return-btn btn" onClick={()=>{desincrementarBarra(); reproducirSonido();}}>
       <img src={rowLeft} />
       Atras
     </Link>
